0ec1e4da61a4b7737d6c4c169aec9984f6106143
karupanerura <karupa@cpan.org>
2018-02-19 14:49:15 +0900

    add description

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 This is simple fork of "golang.org/x/sync/errgroup" to stop using "golang.org/x/net/context".
 
+This repository will ought to be delete when following issues are closed:
+
+- https://github.com/golang/go/issues/19781
+- https://go-review.googlesource.com/c/sync/+/84481
+
 ## Fork Source
 
 https://github.com/golang/sync/tree/fd80eb99c8f653c847d294a001bdf2a3a6f768f5/errgroup
--- errgroup.go
+++ errgroup.go
@@ -6,6 +6,10 @@
 // cancelation for groups of goroutines working on subtasks of a common task.
 //
 // This is simple fork of "golang.org/x/sync/errgroup" to stop using "golang.org/x/net/context".
+//
+// This package will ought to be delete when following issues are closed:
+// - https://github.com/golang/go/issues/19781
+// - https://go-review.googlesource.com/c/sync/+/84481
 package errgroup2
 
 import (
